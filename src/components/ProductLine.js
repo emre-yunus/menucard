143d4400f9eeddd972b4583ef6b307a18ab76b0a
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-13 11:20:46 +0200

    productnote als component

--- src/components/ProductLine.js
+++ src/components/ProductLine.js
@@ -4,11 +4,18 @@ function ProductSize(props) {
     return <span className="productSize"> - {size}cl</span>
 }
 
+function ProductNote(props) {
+    const {note} = props;
+    if (!note) return null;
+    return <div className="note">{note}</div>
+}
+
 export function ProductLine(props) {
     const {product} = props;
     return <div className="productLine">
         <div>{product.name}
             <ProductSize size={product.size} />
+            <ProductNote note={product.note} />
         </div>
         <div>{product.price}</div>
     </div>
