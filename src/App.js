e63181e21bffc3462c3f9fe89e7d5e3cca1fcb2f
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-24 18:19:34 +0200

    normalize en react-bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import 'normalize.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {Category} from "./components/Category";
 import {CATEGORIES_DATA} from "./data/data";
