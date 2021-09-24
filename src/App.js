6fc0c929bc3ae46037b5b4342544a7524f4a3333
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-24 18:58:21 +0200

    NavBar toegevoegd

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {Category} from "./components/Category";
 import {CATEGORIES_DATA} from "./data/data";
-import {Container, Row} from "react-bootstrap";
+import {Container, Navbar, Row} from "react-bootstrap";
 
 function App() {
     return (
-        <Container fluid className="p-0 p-md-3 p-lg-5">
-            <Row className="m-0 mt-4 menu-h1"><h1> Menu</h1></Row>
-            <Row className="m-0">
-                {CATEGORIES_DATA.map(c => <Category category={c} key={c.name} />)}
-            </Row>
-        </Container>
+        <>
+            <Navbar fixed="top" bg="dark" variant="dark">
+                <Navbar.Brand className="p-2">Ons cafe</Navbar.Brand>
+            </Navbar>
+            <Container fluid className="p-0 p-md-3 p-lg-5">
+                <Row className="m-0 mt-4 menu-h1"><h1> Menu</h1></Row>
+                <Row className="m-0">
+                    {CATEGORIES_DATA.map(c => <Category category={c} key={c.name} />)}
+                </Row>
+            </Container>
+        </>
     );
 }
 
