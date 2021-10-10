d101cc2c75bd29ae7dab093ac816eb039c83c981
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-10-10 13:51:53 +0200

    menucard met products uit database

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {CATEGORIES_DATA} from "./data/data";
 import {Container} from "react-bootstrap";
 import {MenuNavbar} from "./components/MenuNavbar";
 import {MenuCard} from "./components/MenuCard";
+import {getProductsFromDb} from "./services/firestoreProducts";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [menuFromDb, setMenuFromDb] = useState([]);
+
+    const loadMenu = async function () {
+        setMenuFromDb(await getProductsFromDb());
+    }
+
+    useEffect(() => loadMenu(), []);
+
     return <Container fluid className="p-0 p-md-3 p-lg-5">
         <MenuNavbar/>
-        <MenuCard menuData={CATEGORIES_DATA}/>
+        <MenuCard menuData={menuFromDb}/>
     </Container>;
-
 }
 
 export default App;
---
+++ src/services/firestoreProducts.js
@@ -0,0 +1,14 @@
+import "firebase/compat/firestore";
+import {firestoreDatabase} from "./firestore";
+
+const COLLECTION_PRODUCTS = "products";
+
+export async function getProductsFromDb() {
+    if (!firestoreDatabase) return [];
+
+    const result = await firestoreDatabase.collection(COLLECTION_PRODUCTS).get();
+    if (result.empty) return [];
+
+    const data = result.docs.map(doc => ({...doc.data(), id: doc.id}));
+    return [...new Set(data.map(p => p.category))].map(c => ({ name: c, products: data.filter(p => p.category===c) }));
+}
